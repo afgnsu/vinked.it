c15cb07313a1faf4fb53547bfbaaaa0516fe4bdd
John van Arkelen <johnvanarkelen@gmail.com>
2014-02-07 16:42:51 +0100

    added proper errors to vink form

--- app/assets/javascripts/general.js
+++ app/assets/javascripts/general.js
@@ -6,23 +6,20 @@ $(document).ready(function(){
   $(document).on('click', '.vink_button', function(e) {
     e.preventDefault();
     var id = this.id;
-    $('#club_'+id).show();
+    $('#club_'+id).toggle();
   });
 
   $(document).on('click', '.vink_close', function(e) {
     e.preventDefault();
     $('.vink_form').hide();
+    $('.errors').empty();
   });
 
   $(document).on('click', '.vink_details', function(e) {
     e.preventDefault();
     var id = this.id;
-    $('#vink_'+id).show();
+    $('#vink_'+id).toggle();
   });
 
-  $(document).on('click', '.vink_details_close', function(e) {
-    e.preventDefault();
-    $('.vink_details_panel').hide();
-  });
 
 });
