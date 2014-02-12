34a72cc96cc327669608fea589fb9eb7b6050722
John van Arkelen <johnvanarkelen@gmail.com>
2014-02-12 19:58:09 +0100

    moved foundation icons

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,6 @@ module VinkedIt
     config.i18n.default_locale = :en
     config.i18n.enforce_available_locales = false
     config.assets.initialize_on_precompile = false
-    #config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
