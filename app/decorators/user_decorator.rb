9002048028a8778bae1f02e821f4f032f4278a72
John van Arkelen <johnvanarkelen@gmail.com>
2014-02-13 14:56:08 +0100

    added club collection

---
+++ app/decorators/user_decorator.rb
@@ -0,0 +1,17 @@
+class ItemDecorator
+  def initialize(item)
+    @item = item
+  end
+
+  def is_featured?
+    @item.ratings > 5
+  end
+
+  def method_missing(method_name, *args, &block)
+    @item.send(method_name, *args, &block)
+  end
+
+  def respond_to_missing?(method_name, include_private = false)
+    @item.respond_to?(method_name, include_private) || super
+  end
+end
---
+++ app/models/collections/club_collection.rb
@@ -0,0 +1,50 @@
+module Collections
+  class ClubCollection < Collection
+
+    module CountryScope
+      def items
+        if params[:view] == "own"
+          super.where("vinks.user_id = ?", current_user.id).uniq
+        elsif params[:view] == "all"
+          super
+        elsif params[:view] == "latest"
+          super.limit(25)
+        else
+          if params[:country].blank?
+            country = Country.where(country: "England").first
+            super.where(country_id: country)
+          else
+            super.where(country_id: params[:country])
+          end
+        end
+      end
+    end
+
+    module Ordering
+      def items
+        if params[:view] == "own" or params[:view] == "latest"
+          super.order("vinks.vink_date DESC")
+        else
+          super.order("name")
+        end
+      end
+    end
+
+    attr_reader :ability, :params
+
+    def initialize(ability, params)
+      @ability = ability
+      @params  = params
+      extend Authorisation, CountryScope, Ordering
+    end
+
+    def items
+      Club.includes(:vinks).includes(:country).page(params[:page])
+    end
+
+    def paginated
+      extend Pagination
+      self
+    end
+  end
+end
