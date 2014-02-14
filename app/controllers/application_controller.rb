783916999f4e5d333a4596d1c3983faafc383c4f
John van Arkelen <johnvanarkelen@gmail.com>
2014-02-14 07:56:57 +0100

    omniauth working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: I18n.t('.general.not_authorized')
   end
 
-  # Let Devise redirect to the last stored URL or the root URL when it has no specific page to
-  # redirect back to. This overrides normal Devise behaviour that would have
-  # it redirect to the users overview.
-  def after_sign_in_path_for(resource)
-    if resource.is_a?(User) && resource.locale.to_sym != I18n.locale
-      I18n.locale = resource.locale.to_sym
-      session[:locale] = I18n.locale
-    end
-    session[:user_return_to] || root_url
-    super
-  end
-
   def set_locale
     I18n.locale = session[:locale]
   end
@@ -28,7 +16,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :screen_name, :location, :email, :locale, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me, :uid, :provider) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :screen_name, :location, :email, :locale, :password, :password_confirmation, :uid, :provider) }
   end
 end
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,7 +3,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = I18n.t('.devise.omniauth_callbacks.success')
-      sign_in_and_redirect user
+      sign_in user
+      redirect_to edit_user_path(user)
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_registration_url, notice: I18n.t('.devise.omniauth_callbacks.failure')
