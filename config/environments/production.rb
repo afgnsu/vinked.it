6c8c2eaab64321f997b090091d80a44f0f809988
John van Arkelen <johnvanarkelen@gmail.com>
2014-04-27 14:56:37 +0200

    latest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,6 @@ VinkedIt::Application.configure do
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
 
-  config.action_mailer.default_url_options = { :host => 'www.vinked.it' }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
