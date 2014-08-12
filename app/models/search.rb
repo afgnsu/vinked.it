0c3470d480fa71ff5364a124fde1bd7cb8f2abf9
John van Arkelen <johnvanarkelen@gmail.com>
2014-08-12 20:05:08 +0200

    updated search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,8 @@
 class Search
 
-  def self.for(keyword)
+  def self.for(keyword, user_id=nil)
+    @vinks = set_vinks_scope(user_id)
+
     if is_number?(keyword)
       @user.vinks.where('vink_nr > ? and vink_nr < ?', keyword.to_i - 4, keyword.to_i + 4).order("vink_date DESC")
     else
@@ -9,14 +11,23 @@ class Search
       clubs = Club.where('lower(name) LIKE ?', keyword_search).pluck(:id)
       leagues = League.where('lower(name) LIKE ?', keyword_search).pluck(:id)
 
-      @user.vinks.where('club_id IN (?)', clubs).order("vink_date DESC") +
-        @user.vinks.where('away_club_id IN (?)', clubs).order("vink_date DESC") +
-        @user.vinks.where('league_id IN (?)', leagues).order("vink_date DESC") +
-        @user.vinks.where('lower(ground) LIKE ?', keyword_search).order("vink_date DESC")
+      @vinks.where('club_id IN (?)', clubs) +
+        @vinks.where('away_club_id IN (?)', clubs) +
+        @vinks.where('league_id IN (?)', leagues) +
+        @vinks.where('lower(ground) LIKE ?', keyword_search)
     end
   end
 
   private
+
+  def self.set_vinks_scope(user_id)
+    if user_id.blank?
+      vinks = Vink
+    else
+      vinks = User.find(user_id).vinks
+    end
+    vinks.order("vink_date DESC")
+  end
 
   def self.is_number?(nr)
     true if Integer nr rescue false
