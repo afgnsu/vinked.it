c5bc5597ac480c62729a8ab3e7228652dbde9149
John van Arkelen <johnvanarkelen@gmail.com>
2014-08-12 19:53:58 +0200

    updated search

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,17 +2,17 @@ class Search
 
   def self.for(keyword)
     if is_number?(keyword)
-      Vink.where('vink_nr > ? and vink_nr < ?', keyword.to_i - 4, keyword.to_i + 4).order("vink_date DESC")
+      @user.vinks.where('vink_nr > ? and vink_nr < ?', keyword.to_i - 4, keyword.to_i + 4).order("vink_date DESC")
     else
       keyword_search = "%#{keyword.downcase}%"
 
       clubs = Club.where('lower(name) LIKE ?', keyword_search).pluck(:id)
       leagues = League.where('lower(name) LIKE ?', keyword_search).pluck(:id)
 
-      Vink.where('club_id IN (?)', clubs).order("vink_date DESC") +
-        Vink.where('away_club_id IN (?)', clubs).order("vink_date DESC") +
-        Vink.where('league_id IN (?)', leagues).order("vink_date DESC") +
-        Vink.where('lower(ground) LIKE ?', keyword_search).order("vink_date DESC")
+      @user.vinks.where('club_id IN (?)', clubs).order("vink_date DESC") +
+        @user.vinks.where('away_club_id IN (?)', clubs).order("vink_date DESC") +
+        @user.vinks.where('league_id IN (?)', leagues).order("vink_date DESC") +
+        @user.vinks.where('lower(ground) LIKE ?', keyword_search).order("vink_date DESC")
     end
   end
 
